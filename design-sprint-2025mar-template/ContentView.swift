748be82c47b721dea56e5819dc18ffb6df0e1024
GSOsborne <gs.osborne.pro@gmail.com>
2025-03-09 18:42:54 -0400

    whoops missed a spot

--- design-sprint-2025mar-template/ContentView.swift
+++ design-sprint-2025mar-template/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
         }
         .padding()
         .task{
-            await
+            //await
             //openImmersiveSpace(id: "physicsball")
         }
     }
