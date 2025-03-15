ec9604f4f84e31fc137beaa83bf1381af043ba71
GSOsborne <gs.osborne.pro@gmail.com>
2025-03-14 23:15:46 -0400

    Fixed gesture for throwing and made target a pysical object.

--- design-sprint-2025mar-template/PhysicsBall.swift
+++ design-sprint-2025mar-template/PhysicsBall.swift
@@ -11,13 +11,19 @@ import RealityKitContent
 import AVFoundation
 
 struct PhysicsBall: View {
+    @State private var startingEntityPosition =
+    SIMD3<Float> (x:0, y:1, z:0)
+    @State private var lastFrameEntityPosition =
+    SIMD3<Float> (x:0, y:1, z:0)
     @State private var currentEntityPosition =
     SIMD3<Float> (x:0, y:1, z:0)
     @State private var resetPosition = SIMD3<Float> (x:1.0, y:1.0, z: -1.5)
     @State private var targetStartPosition = SIMD3<Float> (x:0.0, y:2.0, z: -2.0)
+    @State private var targetScaleMultiplier = Float(0.3)
     @State private var isDragging: Bool = false
     @State private var sphereRadius: Float = 0.3
     @State private var theSphereEntity : ModelEntity?
+    @State private var theFloorEntity : ModelEntity?
     @State private var theTargetEntity : Entity?
     @State private var forceToApply : SIMD3<Float> = .zero
     @State private var subs: [EventSubscription] = []
@@ -28,9 +34,25 @@ struct PhysicsBall: View {
     var body: some View {
         RealityView{ content in
             if let targetContentEntity = try? await Entity(named: "target", in: realityKitContentBundle){
+                
+                targetContentEntity.position = targetStartPosition
+                targetContentEntity.transform.rotation = simd_quatf (angle: -90, axis: SIMD3<Float>(x: 0, y: 1, z: 0))
+                targetContentEntity.scale *= targetScaleMultiplier
+                let targetBoxSize = SIMD3<Float>(x: 0.3, y: 1.6, z: 1.6)
+                let targetShape = ShapeResource.generateBox(size: targetBoxSize)
+                var targetPhysicsBody = PhysicsBodyComponent(
+                    shapes: [targetShape],
+                    density: 10_000
+                )
+                targetPhysicsBody.mode = .kinematic
+                
+                targetContentEntity.components.set(CollisionComponent(shapes: [targetShape]))
+                
+                targetContentEntity.components.set(targetPhysicsBody)
                 theTargetEntity = targetContentEntity
-                theTargetEntity?.position = targetStartPosition
                 content.add(targetContentEntity)
+                
+                
             }
     
         }
@@ -55,9 +77,7 @@ struct PhysicsBall: View {
             let sphereEntity = ModelEntity(mesh:sphereMesh, materials: [material])
             theSphereEntity = sphereEntity
             let shape = ShapeResource.generateSphere(radius: sphereRadius)
-            sphereEntity.position.x = 0.0
-            sphereEntity.position.y = 1.0
-            sphereEntity.position.z = -1.5
+            sphereEntity.position = resetPosition
             
             sphereEntity.components.set(CollisionComponent(shapes: [.generateSphere(radius: 0.3)])
             )
@@ -97,27 +117,33 @@ struct PhysicsBall: View {
                 density: 10_000)
             floorPhysicsBody.isAffectedByGravity = false
             floorEntity.components.set(floorPhysicsBody)
+            theFloorEntity = floorEntity
             content.add(floorEntity)
             
             let subscribe = content.subscribe(to: CollisionEvents.Began.self, on: theSphereEntity) { event in
-                print("Collision between ball and floor, I assume")
-                score = score + 1
-                print("score is: " + score.description)
-                playCollisionSound()
-                theSphereEntity?.position = resetPosition
-                theSphereEntity?.physicsMotion?.linearVelocity = [0.0 , 0.0, 0.0]
-                theSphereEntity?.physicsBody?.isAffectedByGravity = false
-                                }
+                if(event.entityA == self.theFloorEntity || event.entityB == self.theFloorEntity){
+                    print("Collision between ball and floor, I assume")
+                    playCollisionSound()
+                    theSphereEntity?.position = resetPosition
+                    theSphereEntity?.physicsMotion?.linearVelocity = [0.0 , 0.0, 0.0]
+                    theSphereEntity?.physicsBody?.isAffectedByGravity = false
+                }
+                else if(event.entityA == self.theTargetEntity || event.entityB == self.theTargetEntity){
+                    score = score + 1
+                    print("score is: " + score.description)
+                }
+            }
             subs.append(subscribe)
             
         }.gesture(DragGesture().targetedToAnyEntity()
             .onChanged{ value in
                 theSphereEntity?.physicsMotion?.linearVelocity = [0.0 , 0.0, 0.0]
                 if isDragging == false {
-                    currentEntityPosition = value.entity.position
+                    startingEntityPosition = value.entity.position
                     
                     isDragging = true
                 }
+                currentEntityPosition = value.entity.position
                 
             let gestureTranslation =
                 value.convert(
@@ -125,10 +151,10 @@ struct PhysicsBall: View {
                     from: .local,
                     to: .scene
                 )
-                forceToApply = gestureTranslation
-                
+                forceToApply = normalize(currentEntityPosition -  lastFrameEntityPosition)
+                lastFrameEntityPosition = value.entity.position
                 value.entity.position =
-                currentEntityPosition + gestureTranslation
+                startingEntityPosition + gestureTranslation
                 
             }.onEnded{value in
                 isDragging=false
